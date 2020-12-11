149b8c37a9b09e908d66a8c41a39588ac70498c2
mehrabali <52687943+mehrabali@users.noreply.github.com>
2020-12-11 19:30:57 +0600

    Uploaded pulldata.py

---
+++ pulldata.py
@@ -0,0 +1,8 @@
+'''
+    Module 1: Program to pull data from OWD
+    Date created: 11 Dec 2020
+    
+'''
+
+import urllib.request
+urllib.request.urlretrieve ("https://covid.ourworldindata.org/data/owid-covid-data.csv", "covid_data_raw.csv")
