f7fac421ba31fdc272ab7b0ebf37745ac2331b13
fhalamos <felipealamos@gmail.com>
2020-12-15 18:45:38 -0300

    Moved gsheets credentials to folder

--- file_names.py
+++ file_names.py
@@ -5,3 +5,5 @@ HASH_COVID_DATA_FILE = 'data/covid_data_md5.txt'
 RAW_COVID_DATA_FILE = 'data/covid_data_raw.csv'
 
 MANUAL_INPUTS_FILE = 'data/manual_inputs.json'
+
+GSHEETS_CREDENTIALS = 'credentials/client_secret.json'
