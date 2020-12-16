720b4ac223fc493dfe901530ebfd233a33c79377
mehrabali <52687943+mehrabali@users.noreply.github.com>
2020-12-16 16:33:21 +0600

    Updated stats_calculator to export usable country_stats.json
    Updated file_names.py
    Updated stats_calculator.py

--- file_names.py
+++ file_names.py
@@ -7,3 +7,5 @@ RAW_COVID_DATA_FILE = 'data/covid_data_raw.csv'
 MANUAL_INPUTS_FILE = 'data/manual_inputs.json'
 
 GSHEETS_CREDENTIALS = 'credentials/client_secret.json'
+
+COUNTRY_STATS_FILE = 'data/country_stats.json'
