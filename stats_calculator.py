f38e2cc2614b4e9ebe19a7eca6117ee5a667db55
fhalamos <felipealamos@gmail.com>
2021-01-21 13:48:06 -0300

    stats_calculator.py getting link_local_data from manual_inputs.json now

--- stats_calculator.py
+++ stats_calculator.py
@@ -17,15 +17,15 @@ def compute_country_stats(show_prints=False):
 
        # Import csv file
         df0 = pd.read_csv(RAW_COVID_DATA_FILE, usecols= ['continent','location', 'date', 'total_cases', 'new_cases', 'new_cases_smoothed', 'positive_rate', 'population'])
-        
+
         # Drop other countries
         ipalist = ['Paraguay', 'Dominican Republic', 'Colombia', 'Peru', 'Mexico', 'Burkina Faso', 'Mali', 'Sierra Leone', 'Liberia', 'Ghana', "Cote d'Ivoire", 'Nigeria', 'Tanzania', 'Zambia', 'Uganda', 'Rwanda', 'Malawi', 'Kenya', 'Myanmar', 'Philippines', 'Bangladesh']
         df1 = df0[df0.location.isin(ipalist)]
-        
+
         # Sort by date
         df = df1.sort_values(['location', 'date'], ascending=[True, False])
-        
-        
+
+
         # First step calculation
         aggregations = {
             "new_cases": [lambda t: t.head(3).mean(), lambda t: t.head(7).mean()],
@@ -36,16 +36,16 @@ def compute_country_stats(show_prints=False):
             'continent': "first",
             'positive_rate': "first"
         }
-        
+
         caldata = df.groupby('location', as_index=False).agg(aggregations)
-        
+
         # Rename columns
         caldata.columns = ["_".join(x) for x in caldata.columns.ravel()]
-        caldata.rename(columns = {'new_cases_<lambda_0>': 'caseavg_3day', 'new_cases_<lambda_1>': 'caseavg_7day', 
-                                  'new_cases_smoothed_<lambda>': 'caseavg_7dayprev', 'date_first': 'date', 'population_first': 'population', 
+        caldata.rename(columns = {'new_cases_<lambda_0>': 'caseavg_3day', 'new_cases_<lambda_1>': 'caseavg_7day',
+                                  'new_cases_smoothed_<lambda>': 'caseavg_7dayprev', 'date_first': 'date', 'population_first': 'population',
                                   'location_':'location', 'continent_first':'region', 'positive_rate_first':'positive_rate'},
                                   inplace = True)
-        
+
         # Second step calculation
         caldata["growthrate"] = (caldata['caseavg_7day'] - caldata['caseavg_7dayprev']) *100 / caldata['caseavg_7dayprev']
         caldata["cases_per_100000"] = caldata['total_cases_max']*100000 / caldata['population']
@@ -57,66 +57,52 @@ def compute_country_stats(show_prints=False):
         caldata["status_dbl"] = caldata["douberate"].apply(lambda x : 1 if (x >= 10 or x <= 0) else 0)
         caldata["status_casepop"] = caldata["cases_per_100000"].apply(lambda x : 1 if x < 50 else 0)
         caldata["positive_rate_dum"] = caldata["positive_rate"].apply(lambda x : 0 if (x > 0.005) else 1)
-        caldata['statuscode'] = caldata.status_3day.map(str) + caldata.status_dbl.map(str) + caldata.status_casepop.map(str) + caldata.positive_rate_dum.map(str) 
-        
-        
+        caldata['statuscode'] = caldata.status_3day.map(str) + caldata.status_dbl.map(str) + caldata.status_casepop.map(str) + caldata.positive_rate_dum.map(str)
+
+
         # Third step - dashboard
         caldata["case_doubling_rate"] = caldata["douberate"].apply(lambda x : '>100' if (x <0 or x > 100) else round(x, 1))
         caldata["new_cases_per_day"] = caldata['caseavg_3day'].round(0).astype(int)
         caldata['status'] = caldata["statuscode"].apply(lambda x : "Yellow" if x == "1111" else "Red")
         caldata['cases_per_100000'] = caldata['cases_per_100000'].round(1)
-        caldata['link_local_data'] =  caldata["location"].apply(lambda x : "https://covid19.health.gov.mw/" if x == "Malawi" else 
-                                                        ("https://covid19.gou.go.ug/" if x == "Uganda" else
-                                                         ("https://www.sig.gov.bf/infos-covid-19" if x == "Burkina Faso" else
-                                                          ("http://info-covid19.gouv.ci/" if x == "Cote d'Ivoire" else
-                                                           ("https://ghanahealthservice.org/covid19/" if x == "Ghana" else
-                                                            ("https://covid19.ncdc.gov.ng/report/" if x == "Nigeria" else 
-                                                             ("https://sl.one.un.org/covid-19-sitreps/" if x == "Sierra Leone" else
-                                                              ("https://corona.gov.bd/" if x == "Bangladesh" else 
-                                                               ("https://mohs.gov.mm/Main/content/publication/2019-ncov" if x == "Myanmar" else
-                                                                ("https://www.doh.gov.ph/covid19tracker" if x == "Philippines" else 
-                                                                 ("https://www.ins.gov.co/Noticias/Paginas/coronavirus-casos.aspx" if x == "Colombia" else
-                                                                  ("https://coronavirus.gob.mx/datos/" if x == "Mexico" else 
-                                                                   "https://covid19.minsa.gob.pe/sala_situacional.asp" if x == "Peru" else ""))))))))))))
-        
-        
+
         # Final output
-        caldata = caldata[['location', 'region', 'status', 'case_doubling_rate', 'new_cases_per_day', 'cases_per_100000', 'link_local_data']]
+        caldata = caldata[['location', 'region', 'status', 'case_doubling_rate', 'new_cases_per_day', 'cases_per_100000']]
         caldata = caldata.applymap(str)
-        
-        
+
+
         # Import manual_inputs.json
         with open(MANUAL_INPUTS_FILE) as f:
           manual_inputs = json.load(f)
-        
+
         #manual_data = pd.json_normalize(manual_inputs)
         manual_data = pd.DataFrame.from_dict(manual_inputs, orient='index')
         manual_data.reset_index(level=0, inplace=True)
         manual_data.rename(columns = {'index': 'location'}, inplace = True)
-        
+
         # Merge with existing data
-        data = caldata.merge(manual_data, on='location') 
-        
+        data = caldata.merge(manual_data, on='location')
+
         # Override status
         data = data.replace('', np.nan)
         data["override_status"] = data['override_status'].fillna(data['status'])
         del data['status']
-        data.rename(columns = {'override_status': 'status', 
+        data.rename(columns = {'override_status': 'status',
                                'govt.instruction': 'government_restrictions',
                                'outbreak_status': 'subnational_outbreak_status'
                                }
                             , inplace = True)
         data = data.replace(np.nan, '')
 
-        #Remove non-printable chars from columns, given thay are hard to 
-        for col in ['status', 'government_restrictions', 'subnational_outbreak_status']:           
+        #Remove non-printable chars from columns, given thay are hard to print in map
+        for col in ['status', 'government_restrictions', 'subnational_outbreak_status', 'link_local_data']:
             data[col] = data[col].apply(lambda x: ''.join([" " if ord(i) < 32 or ord(i) > 126 else i for i in x]))
 
-        
+
         # Creating dictionary and exporting json
         data.set_index(['location'], inplace = True )
         d=data.to_dict('index')
-        
+
         # Exporting final data file
         with open(COUNTRY_STATS_FILE, "w") as outfile:
             json.dump(d, outfile)
